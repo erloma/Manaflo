afbe70e1faabada87a34d5d7820b57c2cf3463f5
Love Strandäng <lovca@student.chalmers.se>
2025-02-21 13:52:27 +0100

    #3 Add cors for handling frontend requests

--- backend/user-service/cmd/user-service/main.go
+++ backend/user-service/cmd/user-service/main.go
@@ -7,10 +7,17 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gababool/proman/backend/user-service/internal/handlers"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	app := fiber.New()
+
+	// Enable cors for frontend requests
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:5173",
+		AllowMethods: "GET,POST,PUT,DELETE",
+	}))
 
 	app.Get("/ping", handlers.PingHandler)
 
