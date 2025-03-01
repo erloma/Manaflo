b2b27dab24dc6b62e63905ec557f1dc92746a485
Martin Lidgren <marlidg@student.chalmers.se>
2025-03-01 21:38:54 +0100

    #7 Setup initial schema for User

--- backend/user-service/models/models.go
+++ backend/user-service/models/models.go
@@ -4,8 +4,10 @@ import "gorm.io/gorm"
 
 type User struct {
     gorm.Model
-    FirstName  string `json:"firstName"`
-	LastName  string `json:"lastName"`
-    Email string `json:"email" gorm:"unique"`
-	Password string `json:"password"`
+    FirstName string `gorm:"not null;size:16"         json:"firstName"`
+	LastName  string `gorm:"not null;size:25"         json:"lastName"`
+    Email     string `gorm:"not null;size:100;unique" json:"email"`
+	Password  string `gorm:"not null;size:25"         json:"password"`
+    
+    // Profile image?
 }
