8bc9af6969c9e2884577fcef30839277a1cfaca9
Martin Lidgren <marlidg@student.chalmers.se>
2025-03-01 21:40:13 +0100

    #7 Setup inital schema for a project, including roles, members, milestones and attachments

--- backend/project-service/models/models.go
+++ backend/project-service/models/models.go
@@ -1,7 +1,48 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+	"gorm.io/gorm"
+)
+
 
 type Project struct {
     gorm.Model
+    Name        string       `gorm:"not null;size:255" json:"name"`
+    Description string       `gorm:"type:text"         json:"description"`
+    CreatedBy   int          `gorm:"not null"          json:"created_by"`
+    Attachments []Attachment `json:"attachments"`
 }
+
+type Role struct {
+    gorm.Model
+    Project     Project   `gorm:"not null"           json:"project"`           
+    Name        string    `gorm:"not null;size:100"  json:"name"`  
+    Description string    `gorm:"type:text"          json:"description"`
+}
+
+type ProjectMember struct {
+    gorm.Model
+    Project Project   `gorm:"not null"                  json:"project"`
+    User    uint      `gorm:"not null"                  json:"user"`
+    Role    Role      `gorm:"not null;default:'Member"  json:"role"`
+}
+
+type ProjectMilestone struct {
+    ProjectID   Project   `json:"project"`
+    Name        string    `json:"name"` 
+    Description string    `json:"description"`
+    DueDate     time.Time `json:"due_date"`
+    Tasks       []uint    `json:"tasks"`
+}
+
+type Attachment struct {
+    gorm.Model
+    FilePath   string    `json:"file_path"`  
+    FileName   string    `json:"file_name"`
+    FileType   string    `json:"file_type"`
+    UploadedBy uint      `json:"uploaded_by"` 
+    ProjectID    uint    `json:"project_id"`
+}
+
+// Permissions for differing roles?
