bd93feaf4b2e7775ad39f5095abeefbdc76d3f06
Martin Lidgren <marlidg@student.chalmers.se>
2025-03-01 21:40:59 +0100

    #7 Setup initial schema for log entries

--- backend/log-service/models/models.go
+++ backend/log-service/models/models.go
@@ -4,4 +4,7 @@ import "gorm.io/gorm"
 
 type Log struct {
     gorm.Model
+    ServiceName string    `json:"service_name"` 
+    LogLevel    string    `json:"log_level"`   // Add enum here
+    Message     string    `json:"message"`
 }
