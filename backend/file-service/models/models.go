82131b5af51b8306ab0515c0076a09f496a631d2
Martin Lidgren <marlidg@student.chalmers.se>
2025-03-01 21:40:46 +0100

    #7 Setup inital schema for files

--- backend/file-service/models/models.go
+++ backend/file-service/models/models.go
@@ -4,4 +4,9 @@ import "gorm.io/gorm"
 
 type File struct {
     gorm.Model
+    FilePath   string    `json:"file_path"`
+    FileName   string    `json:"file_name"`
+    FileType   string    `json:"file_type"`
+    FileSize   int64     `json:"file_size"` 
+    UploadedBy uint      `json:"uploaded_by"`
 }
