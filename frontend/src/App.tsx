2adaa00fc53ad0967855f4f07329bbcd5e0b8102
Erik Nisbet <eriknis@student.chalmers.se>
2025-03-20 13:00:41 +0100

    #11 Add shadcn components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,12 +9,10 @@ function App() {
   return (
     <div className="p-6">
       <h1 className="text-red-500 text-xl font-bold mb-4">Project Manager</h1>
-      <Accordion type="single" collapsible className="w-full border rounded-lg">
-        <AccordionItem value="item-1" className="border-b">
-          <AccordionTrigger className="p-4 bg-gray-100 hover:bg-gray-200 transition rounded-t-lg">
-            Is it accessible?
-          </AccordionTrigger>
-          <AccordionContent className="p-4 bg-white">
+      <Accordion className="border-2 rounded-2xl"type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
             Yes. It adheres to the WAI-ARIA design pattern.
           </AccordionContent>
         </AccordionItem>
