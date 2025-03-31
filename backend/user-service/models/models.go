fbae4fb554d83cd78c4da4eff6c6d305338a368e
Martin Lidgren <marlidg@student.chalmers.se>
2025-04-01 00:12:12 +0200

    #13 Add a login request model to user-service models

--- backend/user-service/models/models.go
+++ backend/user-service/models/models.go
@@ -3,11 +3,14 @@ package models
 import "gorm.io/gorm"
 
 type User struct {
-    gorm.Model
-    FirstName string `gorm:"not null;size:16"         json:"firstName"`
+	gorm.Model
+	FirstName string `gorm:"not null;size:16"         json:"firstName"`
 	LastName  string `gorm:"not null;size:25"         json:"lastName"`
-    Email     string `gorm:"not null;size:100;unique" json:"email"`
-	Password  string `gorm:"not null;size:25"         json:"password"`
-    
-    // Profile image?
+	Email     string `gorm:"not null;size:100;unique" json:"email"`
+	Password  string `gorm:"not null;size:255"        json:"password"`
+}
+
+type LoginRequest struct {
+	Email    string `gorm:"not null;size:100;unique" json:"email"`
+	Password string `gorm:"not null;size:255"        json:"password"`
 }
