079faae5df068161b6572c2f2de56b02f3179d2e
Erik Nisbet <eriknisbet8@gmail.com>
2025-04-01 17:55:50 +0200

    #14 Keep routes unprotected for now

--- backend/task-service/routes/routes.go
+++ backend/task-service/routes/routes.go
@@ -2,15 +2,15 @@ package routes
 
 import (
     "github.com/gofiber/fiber/v2"
-    "github.com/erloma/manaflo/backend/task-service/middleware"
+    //"github.com/erloma/manaflo/backend/task-service/middleware"
     "github.com/erloma/manaflo/backend/task-service/handlers"
 )
 
 func SetupRoutes(app *fiber.App) {
-
-
-    // Protected routes (below this point)
-    api := app.Group("/api", middleware.AuthRequired())
+    
+    /*api := app.Group("/api", middleware.AuthRequired())
     api.Post("/api/tasks", handlers.CreateTask)
-    api.Get("/api/tasks", handlers.GetTasks)
+    api.Get("/api/tasks", handlers.GetTasks)*/
+    app.Post("/api/tasks", handlers.CreateTask)
+    app.Get("/api/tasks", handlers.GetTasks)
 }
