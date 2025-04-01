953c171de1abe46f502a10016b56a48de290c377
Love Strandäng <lovca@student.chalmers.se>
2025-04-01 16:55:07 +0200

    Add testify dependency to use assert methods and create test for GetProjects normal

--- backend/project-service/handlers/project_handler_test.go
+++ backend/project-service/handlers/project_handler_test.go
@@ -5,10 +5,12 @@ import (
 	"encoding/json"
 	"net/http/httptest"
 	"testing"
+
 	"github.com/erloma/manaflo/backend/project-service/config"
 	"github.com/erloma/manaflo/backend/project-service/models"
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCreateProject(t *testing.T) {
@@ -43,3 +45,59 @@ func TestCreateProject(t *testing.T) {
 	}
 }
 
+func TestGetProjects(t *testing.T) {
+	// Load env
+	err := godotenv.Load("../.env")
+	if err != nil {
+		t.Fatal("Couldnt load env file")
+	}
+
+	// Connect to and migrate DB
+	db, err := config.GetDB()
+	if err != nil {
+		t.Fatal("Failed to connect to database")
+	}
+	db.AutoMigrate(&models.Project{})
+
+	// Create a test project to get from DB
+	testProject := models.Project{Name: "ProjectTestName", Description: "Description", CreatedBy: 2}
+	db.Create(&testProject)
+
+	// Setup app and route
+	app := fiber.New()
+	app.Get("/projects", GetProjects)
+
+	// Simulate GET request
+	req := httptest.NewRequest("GET", "/projects", nil)
+	req.Header.Set("Content-type", "application/json")
+
+	resp, err := app.Test(req)
+	if err != nil {
+		t.Fatalf("Error sending request: %v", err)
+	}
+	assert.Equal(t, fiber.StatusOK, resp.StatusCode)
+
+	// Decode resp body
+	var projects []models.Project
+	err = json.NewDecoder(resp.Body).Decode(&projects)
+	if err != nil {
+		t.Fatalf("error decoding body from JSON")
+	}
+
+	// Assert at least one project is returned
+	assert.GreaterOrEqual(t, len(projects), 1)
+
+	foundBool := false
+	// Check that a project with the test name exists
+	for _, p := range projects {
+		if p.Name == testProject.Name && p.CreatedBy == testProject.CreatedBy && p.Description == testProject.Description {
+			foundBool = true
+			break
+		}
+	}
+	assert.True(t, foundBool, "Test project not found in the response")
+
+	// Clear the DB after the test
+	db.Where("name = ?", "ProjectTestName").Delete(&models.Project{})
+
+}
