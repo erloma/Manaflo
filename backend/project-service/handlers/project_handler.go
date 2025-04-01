24805d4bd831c558da2647040319c77c1c384083
Love Strandäng <lovca@student.chalmers.se>
2025-04-01 16:24:04 +0200

    Add input validation in createProject to prevent projects with invalid fields being created

--- backend/project-service/handlers/project_handler.go
+++ backend/project-service/handlers/project_handler.go
@@ -1,43 +1,48 @@
 package handlers
 
 import (
+	"github.com/erloma/manaflo/backend/project-service/config"
 	"github.com/erloma/manaflo/backend/project-service/models"
-    "github.com/erloma/manaflo/backend/project-service/config"
 	"github.com/gofiber/fiber/v2"
 )
 
-
 func CreateProject(c *fiber.Ctx) error {
-    db, err := config.GetDB()
-    if err != nil {
-        return c.Status(500).JSON(fiber.Map{"error": "Database connection failed"})
+	db, err := config.GetDB()
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": "Database connection failed"})
+	}
+
+	project := new(models.Project)
+
+	if err := c.BodyParser(project); err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": "Unable to parse JSON"})
+	}
+
+    if project.Name == "" || len(project.Name) > 255 || project.CreatedBy <= 0 {
+        return c.Status(400).JSON(fiber.Map{"error": "Invalid fields in project entered"})
     }
 
-    project := new(models.Project)
+    // TODO add check if createdBy ID maps to a valid user
 
-    if err := c.BodyParser(project); err != nil {
-        return c.Status(500).JSON(fiber.Map{"error": "Unable to parse JSON"})
-    }
+	if err := db.Create(project).Error; err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": "Unable to create project"})
+	}
 
-    if err := db.Create(project).Error; err != nil {
-        return c.Status(500).JSON(fiber.Map{"error": "Unable to create project"})
-    }
-    
-    return c.JSON(project)
+	return c.JSON(project)
 }
 
 func GetProjects(c *fiber.Ctx) error {
-    db, err := config.GetDB()
-    if err != nil {
-        return c.Status(500).JSON(fiber.Map{"error": "Database connection failed"})
-    }
+	db, err := config.GetDB()
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": "Database connection failed"})
+	}
 
-    projects := new([]models.Project)
+	projects := new([]models.Project)
 
-    if err := db.Find(projects).Error; err != nil {
-        return c.Status(500).JSON(fiber.Map{"error": "Unable to fetch projects"})
-    }
+	if err := db.Find(projects).Error; err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": "Unable to fetch projects"})
+	}
 
-    return c.JSON(projects);
+	return c.JSON(projects)
 
 }
