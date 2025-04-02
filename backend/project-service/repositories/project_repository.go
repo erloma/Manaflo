1af57844282a39ae128e38590e53bd5fd13ebf86
Love Strandäng <lovca@student.chalmers.se>
2025-04-02 11:25:02 +0200

    Create initial project repository

---
+++ backend/project-service/repositories/project_repository.go
@@ -0,0 +1,27 @@
+package repositories
+
+import(
+
+)
+
+type ProjectService struct{}
+
+func NewProjectService() *ProjectService {
+	return &ProjectService{}
+}
+
+func CreateProject(s *ProjectService){
+	
+}
+
+func GetProjects(){
+
+}
+
+func getProjectByID(){
+
+}
+
+func getUserProjects(){
+
+}
