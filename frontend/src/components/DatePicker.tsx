cc01b9db09fef0bb618c1a913299da8ef085eeb3
Erik Nisbet <eriknisbet8@gmail.com>
2025-04-06 10:48:55 +0200

    #14 Make date before current day unable to be picked

--- frontend/src/components/DatePicker.tsx
+++ frontend/src/components/DatePicker.tsx
@@ -4,12 +4,17 @@ import React from "react"
  
 export function DatePicker() {
   const [date, setDate] = React.useState<Date | undefined>(new Date())
+
+  let currentDate = new Date();
+  currentDate.setHours(0,0,0,0);
+
  
   return (
     <Calendar
       mode="single"
       selected={date}
       onSelect={setDate}
+      disabled={{before: currentDate}}
       className="rounded-md border shadow"
     />
   )
