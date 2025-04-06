317545ae410cb35bbaceace6b00b7af04d429180
Erik Nisbet <eriknisbet8@gmail.com>
2025-04-06 10:30:31 +0200

    #14 Commit current changes in backend (broken atm) to be able to merge with main

--- backend/task-service/handlers/task_handler.go
+++ backend/task-service/handlers/task_handler.go
@@ -4,7 +4,25 @@ import (
     "github.com/gofiber/fiber/v2"
     "github.com/erloma/manaflo/backend/task-service/config"
     "github.com/erloma/manaflo/backend/task-service/models"
+    "github.com/erloma/manaflo/backend/task-service/services"
 )
+
+type TaskHandler struct {
+    taskService *services.TaskService
+}
+
+func new TaskHandler(taskService *services.TaskService) *TaskHandler {
+    return &UserHandler(userService: userService)
+}
+
+func (h *TaskHandler) GetTasks(c *fiber.Ctx) error {
+    tasks, err := h.taskService.GetTasks()
+    if err != nil {
+        return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+    }
+    return c.JSON(tasks)
+}
+
 
 
 func CreateTask(c *fiber.Ctx) error {
---
+++ backend/task-service/services/task_service.go
@@ -0,0 +1,7 @@
+package services
+
+import (
+	"errors"
+	"github.com/erloma/manaflo/backend/task-service/models"
+	"github.com/erloma/manaflo/backend/task-service/repositories"
+)
