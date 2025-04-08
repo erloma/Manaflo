88f842bc0299e8160fd7aa9550867da63b17c72e
Erik Nisbet <eriknis@student.chalmers.se>
2025-04-08 19:53:26 +0200

    #14 Add state handler for priority in CreateTaskForm

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    hmr: {
+      protocol: 'ws',
+      host: 'localhost',
+      port: 5173
+    },
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
