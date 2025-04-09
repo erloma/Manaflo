70aefd430cd70d0c2d85b681634ef281b86cfcec
Erik Nisbet <eriknisbet8@gmail.com>
2025-04-09 14:28:59 +0200

    #14 Add repositories in task-service

---
+++ backend/repositories/task_repository.go
@@ -0,0 +1,34 @@
+package repositories
+
+import (
+		"errors"
+		"github.com/erloma/manaflo/backend/task-service/config"
+		"github.com/erloma/manaflo/backend/task-service/models"
+)
+
+func CreateTask(task models.Task) (models.Task, error) {
+	db, err := config.GetDB()
+	if err != nil {
+		return models.User{}, err
+	}
+	result := db.Create(&task)
+	if result.Error != nil {
+		return models.Task{}, result.Error
+	}
+	return task, nil
+}
+// Refactor later to only query tasks in project ID
+func getTasks() ([]models.Task, error) {
+	db, err := config.GetDB()
+	if err != nil {
+		return nil, err
+	}
+
+	var tasks []models.Task
+	result := db.Find(&tasks)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return tasks, nil
+}	
