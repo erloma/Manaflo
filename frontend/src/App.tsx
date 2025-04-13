71a1c1e90732842b32f933d6edf45730915f605c
Martin Lidgren <marlidg@student.chalmers.se>
2025-04-13 11:45:09 +0200

    #13 Create and add a registration page to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import FirstPage from "./pages/FirstPage"
 import LoginPage from "./pages/LoginPage.tsx";
+import RegisterPage from "./pages/RegisterPage.tsx";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       </Route>
       <Route path="first" element={<FirstPage />} />
       <Route path="login" element={<LoginPage />} />
+      <Route path="register" element={<RegisterPage />} />
     </Routes>
   </BrowserRouter>
   );
