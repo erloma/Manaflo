7ce0499f581c93590732b4a7ef3c19e46f9ead03
Erik Nisbet <eriknisbet8@gmail.com>
2025-04-28 12:46:44 +0200

    #25 Last changes

--- frontend/src/lib/api/services/users.ts
+++ frontend/src/lib/api/services/users.ts
@@ -40,7 +40,7 @@ export const updateUserPasswordService = (
 
   export const registerUserService = async ( data: RegisterRequest) : Promise<Response> => {
     return fetch(`${API_URL}/users`, {
-        method: "PATCH",
+        method: "POST",
         headers: { "Content-Type": "application/json"},
         body: JSON.stringify(data),
     });
