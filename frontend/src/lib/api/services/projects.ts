3423c1fbf33cad7a801430e4a40fde6797e73c25
Erik Nisbet <eriknisbet8@gmail.com>
2025-04-28 14:06:58 +0200

    #16 Fix api url

--- frontend/src/lib/api/services/projects.ts
+++ frontend/src/lib/api/services/projects.ts
@@ -2,7 +2,7 @@ import { CreateProjectRequest } from "../types/project";
 const API_URL = "http://localhost:8082/api"
 
 export const createProjectService = (data: CreateProjectRequest) : Promise<Response> => {
-    return fetch(`${API_URL}`, {
+    return fetch(`${API_URL}/projects`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
