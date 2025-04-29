7d1e8de34d7ca0b8e29f65174580e9d5335f4303
Erik Nisbet <eriknisbet8@gmail.com>
2025-04-29 10:42:46 +0200

    #22 Add auth in project-service

--- backend/project-service/routes/routes.go
+++ backend/project-service/routes/routes.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"github.com/erloma/manaflo/backend/project-service/handlers"
 	"github.com/erloma/manaflo/backend/project-service/services"
+	"github.com/gofiber/fiber/middleware"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -11,4 +12,6 @@ func SetupRoutes(app *fiber.App) {
 	h := handlers.NewProjectHandler(svc)
 	app.Post("/api/projects", h.CreateProject)
 	app.Get("/api/projects", h.GetProjects)
+	api := app.Group("/api", middleware.AuthRequired())
+	api.Get("/projects/users", h.GetProjectUsers)
 }
